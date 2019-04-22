c548d3c680de5f0f9eb3f700a2d75487ea3c762e
Leandro Alsberg <leandro.alsberg@gmail.com>
2019-04-22 00:07:04 -0300

    prepare to SPA

--- src/App.vue
+++ src/App.vue
@@ -1,58 +1,22 @@
 <template>
-	<div id="app">
-		<input type="search" class="filter" placeholder="Filter solutions" v-on:input="filter = $event.target.value">
-		{{ filter }}
+	<div>
+		<input type="text" v-on:input="texto = $event.target.value">
 
-		<div class="solution-panel" v-for="solution in filteredSolutions">
-			<the-solution :solution="solution"/>
-		</div>
+		{{ texto }}
 	</div>
 </template>
 
 
 <script>
-	import Solution from './components/Solution.vue';
-
 	export default {
-		name: 'app',
-
-	    components: {
-	        'the-solution': Solution
-	    },
-
 		data () {
 			return {
-				solutions: [],
-				filter: ''
+				texto: ''
 			}
 		},
-
-		computed: {
-			filteredSolutions() {
-				if (!this.filter) {
-					return this.solutions;
-				}
-				let exp = new RegExp(this.filter.trim(), 'i');
-				return this.solutions.filter(solution => exp.test(solution.title));
-			}
-		},
-
-		created() {
-			this.$http.get('http://localhost:8080/resolutions')
-	      		.then(res => res.json())
-	      		.then(solutions => this.solutions = solutions, err => console.log(err));
-		}
 	}
 </script>
 
 
 <style>
-	h1 {
-		font-weight: normal;
-	}
-
-	.filter {
-		display: block;
-		width: 100%;
-	}
 </style>
